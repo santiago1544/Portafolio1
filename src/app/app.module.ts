31b0640ce538cb1c03475c9f0c1b556f48c163ca
santiago1544 <sandovalsanty@outlook.com>
2023-09-04 16:16:07 -0500

    portafolio1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RuletaComponent } from './components/ruleta/ruleta.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RuletaComponent,
+    NavbarComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/ruleta/ruleta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ruleta',
+  templateUrl: './ruleta.component.html',
+  styleUrls: ['./ruleta.component.css']
+})
+export class RuletaComponent {
+
+}
